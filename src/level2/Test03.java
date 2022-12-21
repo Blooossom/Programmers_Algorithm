a62143bde40168450e99d112f0ddbeda7a398bd3
Blooossom <taegyeong19@gmail.com>
2022-12-21 17:37:53 +0900

    feat:Add Programmers Algorithm repo

---
+++ src/level2/Test03.java
@@ -0,0 +1,49 @@
+package level2;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Test03 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] pr = {95, 90, 99, 99, 80, 99};
+        int[] sp = {1,1,1,1,1,1};
+        System.out.println(Arrays.toString(solution(pr, sp)));
+    }
+    static int[] solution(int[] progresses, int[] speeds) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        int day = 1;
+        int k = 0;
+        int count = 0;
+            label2:
+            while(true){
+                if(progresses[k] + (speeds[k] * day) >= 100){
+                    count++;
+                    k++;
+                    while (true) {
+                        if(k >= progresses.length){
+                            break;
+                        }
+                        if(progresses[k] + (speeds[k] * day) >= 100){
+                            k++;
+                            count++;
+                        } else {
+                            break;
+                        }
+                    }
+                    list.add(count);
+                    count = 0;
+                    if (k >= progresses.length) {
+                        break label2;
+                    }
+                } else {
+                    day++;
+                }
+            }
+        int[] answer;
+        return  answer = list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
